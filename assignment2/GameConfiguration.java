1e4f46bb26af17efcacb68dc7624ee383971675f
sammychien <32807210+sammychien@users.noreply.github.com>
2018-09-26 17:21:06 -0500

    Add files via upload

---
+++ assignment2/GameConfiguration.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+/*  
+ * EE422C Project 2 (Mastermind) 
+ * Summer 2016
+ */
+public class GameConfiguration {
+	public static final int guessNumber = 12;
+	public static final String[] colors = {"B","G","O","P","R","Y"};
+	public static final int pegNumber = 4;
+//	public static final String[] colors = {"A", "B", "C", "D", "E", "F"};
+//	public static final int pegNumber = 8;
+}
